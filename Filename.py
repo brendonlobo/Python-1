d54b27121e5094c1b742eed7c19de7da2024c614
Valynn Lobo <65067807+ValynnLobo@users.noreply.github.com>
2020-05-09 15:23:13 +0530

    Create Filename.py

---
+++ Filename.py
@@ -0,0 +1,11 @@
+name=str(input("Input the filename:"))
+if ".py" in name:
+  print("The extension of the file is: python")
+elif ".java" in name:
+  print("The extension of the file is:java")
+elif ".c" in name:
+  print("The extension of the file is:c")
+elif ".cs" in name:
+  print("The extension of the file is:c#")
+elif ".html" in name:
+  print("The extension of the file is:html")
